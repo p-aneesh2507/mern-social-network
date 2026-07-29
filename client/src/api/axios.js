e79de92bca27a2f9e363c87469320107c243af47
Pepakayala Aneesh <pepakayalaaneesh@gmail.com>
2026-07-30 00:40:37 +0530

    Refactor axios base URL to use environment variable

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,42 +1,21 @@
 import axios from "axios";
 
-
 const API = axios.create({
-
-    baseURL:"http://localhost:5000/api"
-
+    baseURL: import.meta.env.VITE_API_URL
 });
 
-
 // Attach token automatically
-
 API.interceptors.request.use(
-
-    (config)=>{
-
+    (config) => {
         const token = localStorage.getItem("token");
 
-
-        if(token){
-
-            config.headers.Authorization =
-            `Bearer ${token}`;
-
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
 
-
         return config;
-
     },
-
-
-    (error)=>{
-
-        return Promise.reject(error);
-
-    }
-
+    (error) => Promise.reject(error)
 );
-
 
 export default API;
